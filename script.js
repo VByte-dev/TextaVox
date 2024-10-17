7023eb1ac4c73306e1116ae407892469692fb461
Vedhesh <vbyte.02@gmail.com>
2024-10-17 23:53:12 +0530

    Completed

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 let character;
 let chrB;
 // Default Character
-document.querySelector(".chr1").style.backgroundColor = '#fbca1f';
+document.querySelector(".chr1").style.backgroundColor = "#fbca1f";
 document.querySelector(".chr1").addEventListener("click", () => {
   character = "";
   character = "Dan";
 
-  document.querySelector(".chr1").style.backgroundColor = '#fbca1f';
-  document.querySelector(".chr2").style.backgroundColor = 'white';
-  document.querySelector(".chr3").style.backgroundColor = 'white';
-  document.querySelector(".chr4").style.backgroundColor = 'white';
+  document.querySelector(".chr1").style.backgroundColor = "#fbca1f";
+  document.querySelector(".chr2").style.backgroundColor = "white";
+  document.querySelector(".chr3").style.backgroundColor = "white";
+  document.querySelector(".chr4").style.backgroundColor = "white";
   // console.log("Dan is selected");
 });
 document.querySelector(".chr2").addEventListener("click", () => {
   character = "";
   character = "Will";
 
-  document.querySelector(".chr1").style.backgroundColor = 'white';
-  document.querySelector(".chr2").style.backgroundColor = '#fbca1f';
-  document.querySelector(".chr3").style.backgroundColor = 'white';
-  document.querySelector(".chr4").style.backgroundColor = 'white';
+  document.querySelector(".chr1").style.backgroundColor = "white";
+  document.querySelector(".chr2").style.backgroundColor = "#fbca1f";
+  document.querySelector(".chr3").style.backgroundColor = "white";
+  document.querySelector(".chr4").style.backgroundColor = "white";
   // console.log("Will is selected");
 });
 document.querySelector(".chr3").addEventListener("click", () => {
   character = "";
   character = "Scarlett";
 
-  document.querySelector(".chr1").style.backgroundColor = 'white';
-  document.querySelector(".chr2").style.backgroundColor = 'white';
-  document.querySelector(".chr3").style.backgroundColor = '#fbca1f';
-  document.querySelector(".chr4").style.backgroundColor = 'white';
+  document.querySelector(".chr1").style.backgroundColor = "white";
+  document.querySelector(".chr2").style.backgroundColor = "white";
+  document.querySelector(".chr3").style.backgroundColor = "#fbca1f";
+  document.querySelector(".chr4").style.backgroundColor = "white";
   // console.log("Scarlett is selected");
 });
 document.querySelector(".chr4").addEventListener("click", () => {
   character = "";
   character = "Liv";
 
-  document.querySelector(".chr1").style.backgroundColor = 'white';
-  document.querySelector(".chr2").style.backgroundColor = 'white';
-  document.querySelector(".chr3").style.backgroundColor = 'white';
-  document.querySelector(".chr4").style.backgroundColor = '#fbca1f';
+  document.querySelector(".chr1").style.backgroundColor = "white";
+  document.querySelector(".chr2").style.backgroundColor = "white";
+  document.querySelector(".chr3").style.backgroundColor = "white";
+  document.querySelector(".chr4").style.backgroundColor = "#fbca1f";
   // console.log("Liv is selected");
 });
 
 document.querySelector("#generateB").addEventListener("click", () => {
-  document.querySelector('.loading').style.display = "flex";
-  setTimeout(()=>{
-    document.querySelector('.loading').style.display = "none";
-  }, 3000)
+  document.querySelector(".loading").style.display = "flex";
+  setTimeout(() => {
+    document.querySelector(".loading").style.display = "none";
+  }, 3000);
   let speed = document.querySelector("#speedB").value;
   // console.log(`Speed is ${speed}`);
   let pitch = document.querySelector("#pitchB").value;
@@ -82,21 +82,20 @@ document.querySelector("#generateB").addEventListener("click", () => {
     // Correcting the Text content
     // console.log(textContent);
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-        Authorization:
-          "gKCKisndVjP0mWjxQInOITOql5pZfZGJ61Mz7hzB5SgiiDiK37NyqK",
+        accept: 'application/json',
+        'content-type': 'application/json',
+        Authorization: 'Bearer gKCKisndVjP0mWjxQInOITOql5pZfZGJ61Mz7hzB5SgiiDiK37NyqK'
       },
       body: JSON.stringify({
         Text: textContent,
         VoiceId: character,
-        Bitrate: "192k",
-        Speed: Number(speed),
-        Pitch: Number(pitch),
-        TimestampType: "sentence",
-      }),
+        Bitrate: '192k',
+        Speed: speed,
+        Pitch: pitch,
+        TimestampType: 'sentence'
+      })
     };
     try {
       const fData = await fetch(
@@ -109,22 +108,22 @@ document.querySelector("#generateB").addEventListener("click", () => {
       // console.log(audioFile);
       document.querySelector("#audio").src = audioFile;
       document.querySelector("#audioD").href = audioFile;
-    } catch {
-      confirm(`Make sure you have used your API Key`);
+    } catch (error) {
+      confirm(`Make sure you have used your API Key ${error}`);
       location.reload();
     }
   };
   ttos();
 });
-document.querySelector('#playB').addEventListener('click', ()=>{
+document.querySelector("#playB").addEventListener("click", () => {
   document.querySelector("audio").play();
 });
-document.querySelector('#pauseB').addEventListener('click', ()=>{
+document.querySelector("#pauseB").addEventListener("click", () => {
   document.querySelector("audio").pause();
 });
-document.querySelector('#downloadB').addEventListener('click', ()=>{
-  document.querySelector("#audioD").download = 'TextaVox.mp3';
+document.querySelector("#downloadB").addEventListener("click", () => {
+  document.querySelector("#audioD").download = "TextaVox.mp3";
 });
-setTimeout(()=>{
-  document.querySelector('.entry').style.display = 'none';
-},1000);
+setTimeout(() => {
+  document.querySelector(".entry").style.display = "none";
+}, 1000);
